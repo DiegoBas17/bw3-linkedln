73c2045f9cbe43e9830e9c62ec6a3141b2c5cec8
angelolaera <angelo.laera.1995@gmail.com>
2024-07-15 16:17:06 +0200

    Navbar update

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import "./App.css";
 import CentralMainHome from "./components/CentralMainHome";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ProfilePage from "./components/ProfilePage";
+import TopNavbar from "./components/TopNavbar";
 
 function App() {
   return (
     <BrowserRouter>
+      <TopNavbar />
       <Routes>
         <Route path="/" element={<ProfilePage />} />
         <Route path="/centralhome" element={<CentralMainHome />} />
