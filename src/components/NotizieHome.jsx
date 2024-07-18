2265ee17cfcf5aa72fbe991793d2c6a690994706
DiegoBas17 <167855623+DiegoBas17@users.noreply.github.com>
2024-07-18 13:39:44 +0200

    add fetch delete and put

--- src/components/NotizieHome.jsx
+++ src/components/NotizieHome.jsx
@@ -1,7 +1,9 @@
 import { useDispatch, useSelector } from "react-redux";
 import { fetchPostNotizieAction } from "../redux/actions/arrayPostHomeAction";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Trash3 } from "react-bootstrap-icons";
+import { API_KEY } from "../redux/actions/getUserAction";
+import { Button, Form, Modal } from "react-bootstrap";
 
 const NotizieHome = () => {
   const dispatch = useDispatch();
@@ -31,57 +33,146 @@ const NotizieHome = () => {
     }
   };
 
+  const deletePost = (postId) => {
+    fetch("https://striveschool-api.herokuapp.com/api/posts/" + postId, {
+      method: "DELETE",
+      headers: {
+        Authorization: API_KEY,
+      },
+    })
+      .then((resp) => {
+        if (resp.ok) {
+          alert("commento eliminato");
+          dispatch(fetchPostNotizieAction()); /* Richiamo dispatch(
+            fetchPostNotizieAction() per aggiornare la lista dei commenti */
+        } else {
+          throw new Error("Errore nel reperimento dei commenti");
+        }
+      })
+      .catch((err) => alert(err));
+  };
+  const [post, setPost] = useState("");
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = (post) => {
+    setPost(post);
+    setShow(true);
+  };
+  console.log(post);
+
+  const editPost = async (postId, textPost) => {
+    try {
+      const resp = await fetch(
+        "https://striveschool-api.herokuapp.com/api/posts/" + postId,
+        {
+          method: "PUT",
+          body: JSON.stringify({ text: textPost }),
+          headers: {
+            Authorization: API_KEY,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (resp.ok) {
+        alert("post modificato correttamente.");
+        dispatch(fetchPostNotizieAction());
+      } else {
+        throw new Error("Errore nel riperimento dei dati");
+      }
+    } catch (error) {
+      console.log("Errore", error);
+    }
+  };
+
+  const handleEditPost = () => {
+    editPost(post._id, post.text);
+    handleClose();
+  };
+
   return (
-    <div>
-      {arrayPostNotizie.toReversed().map((post, index) => {
-        return (
-          <div
-            key={index}
-            className="p-3 border border-1 rounded-3 mt-3 bg-white"
-          >
-            <div className="d-flex justify-content-between align-items-center">
-              <h5>{post.username}</h5>
-              {userMeId === post.user._id && (
-                <div>
-                  <div className="d-inline me-2">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      height="21px"
-                      width="21px"
-                      id="edit-small"
-                      aria-hidden="true"
-                      role="none"
-                      data-supported-dps="16x16"
-                      fill="currentColor"
-                    >
-                      <path d="M14.13 1.86a3 3 0 00-4.17 0l-7 7L1 15l6.19-2 6.94-7a3 3 0 000-4.16zm-8.36 9.71l-1.35-1.34L9.64 5 11 6.35z" />
-                    </svg>
+    <>
+      <div>
+        {arrayPostNotizie.toReversed().map((post, index) => {
+          return (
+            <div
+              key={index}
+              className="p-3 border border-1 rounded-3 mt-3 bg-white"
+            >
+              <div className="d-flex justify-content-between align-items-center">
+                <h5>{post.username}</h5>
+                {userMeId === post.user._id && (
+                  <div>
+                    <div className="d-inline me-2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        height="21px"
+                        width="21px"
+                        id="edit-small"
+                        aria-hidden="true"
+                        role="none"
+                        data-supported-dps="16x16"
+                        fill="currentColor"
+                        onClick={() => handleShow(post)}
+                      >
+                        <path d="M14.13 1.86a3 3 0 00-4.17 0l-7 7L1 15l6.19-2 6.94-7a3 3 0 000-4.16zm-8.36 9.71l-1.35-1.34L9.64 5 11 6.35z" />
+                      </svg>
+                    </div>
+                    <Trash3
+                      fill="red"
+                      onClick={() => {
+                        deletePost(post._id);
+                      }}
+                    />
                   </div>
-                  <Trash3 fill="red" />
-                </div>
-              )}
+                )}
+              </div>
+              <img src={post.user?.image} alt="" height={25} />
+              <p>
+                {timeTrascorso(post.createdAt)}
+                &middot;
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  data-supported-dps="16x16"
+                  fill="currentColor"
+                  width="16"
+                  height="16"
+                  focusable="false"
+                >
+                  <path d="M8 1a7 7 0 107 7 7 7 0 00-7-7zM3 8a5 5 0 011-3l.55.55A1.5 1.5 0 015 6.62v1.07a.75.75 0 00.22.53l.56.56a.75.75 0 00.53.22H7v.69a.75.75 0 00.22.53l.56.56a.75.75 0 01.22.53V13a5 5 0 01-5-5zm6.24 4.83l2-2.46a.75.75 0 00.09-.8l-.58-1.16A.76.76 0 0010 8H7v-.19a.51.51 0 01.28-.45l.38-.19a.74.74 0 01.68 0L9 7.5l.38-.7a1 1 0 00.12-.48v-.85a.78.78 0 01.21-.53l1.07-1.09a5 5 0 01-1.54 9z" />
+                </svg>
+              </p>
+              <p>{post.text}</p>
             </div>
-            <img src={post.user?.image} alt="" height={25} />
-            <p>
-              {timeTrascorso(post.createdAt)}
-              &middot;
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 16 16"
-                data-supported-dps="16x16"
-                fill="currentColor"
-                width="16"
-                height="16"
-                focusable="false"
-              >
-                <path d="M8 1a7 7 0 107 7 7 7 0 00-7-7zM3 8a5 5 0 011-3l.55.55A1.5 1.5 0 015 6.62v1.07a.75.75 0 00.22.53l.56.56a.75.75 0 00.53.22H7v.69a.75.75 0 00.22.53l.56.56a.75.75 0 01.22.53V13a5 5 0 01-5-5zm6.24 4.83l2-2.46a.75.75 0 00.09-.8l-.58-1.16A.76.76 0 0010 8H7v-.19a.51.51 0 01.28-.45l.38-.19a.74.74 0 01.68 0L9 7.5l.38-.7a1 1 0 00.12-.48v-.85a.78.78 0 01.21-.53l1.07-1.09a5 5 0 01-1.54 9z" />
-              </svg>
-            </p>
-            <p>{post.text}</p>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modifica post</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Control
+                type="text"
+                value={post?.text}
+                autoFocus
+                onChange={(e) => setPost({ ...post, text: e.target.value })}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleEditPost}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 export default NotizieHome;
